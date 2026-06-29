dfea54d27fb90037f0fcf58689666f709e690529
famefule143 <famefule143@gmail.com>
2026-06-29 20:27:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("FeatureVa");
+window.addEventListener("load",()=>{
+
+document.body.style.opacity="0";
+
+setTimeout(()=>{
+document.body.style.transition="1s";
+document.body.style.opacity="1";
+},200);
+
+});
+
+const cards=document.querySelectorAll(".card");
+
+window.addEventListener("scroll",()=>{
+
+cards.forEach(card=>{
+
+const top=card.getBoundingClientRect().top;
+
+if(top<window.innerHeight-100){
+
+card.style.opacity="1";
+card.style.transform="translateY(0px)";
+
+}
+
+});
+
+}); 
